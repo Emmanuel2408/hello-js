6dfec34d305cedba50ed0355353963922d13aa6f
Buari Emmanuel <buariemmanuel717@gmail.com>
2026-03-14 12:28:02 +0100

    Initial commit: Hello world project

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Buari Emmanuel"
+    const age = 17; 
+
+
+ console.log("Hello,", "My name is", name, 
+    "and I am", age, "years old");
+console.log("I love html and python as my favourite programming language");
